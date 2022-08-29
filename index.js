fb61781c4410e74f1c7d0f0c6640b2b9c9f9fd93
danielpqb <danielpqb@gmail.com>
2022-08-29 15:44:50 -0300

    Get User Tweets

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/sign-up", (req, res) => {
   }
 
   users.push(user);
-  res.status(201).send("Usuário cadastrado com sucesso.");
+  res.status(201).send("OK. Usuário cadastrado com sucesso.");
 });
 
 //Post Tweet
@@ -36,12 +36,27 @@ app.post("/tweets", (req, res) => {
   const id = tweets.length + 1;
 
   tweets.push({ id, ...tweet, avatar });
-  res.status(201).send("Tweet postado com sucesso.");
+  res.status(201).send("OK. Tweet postado com sucesso.");
 });
 
 //Get Tweets
 app.get("/tweets", (req, res) => {
-  res.status(201).send(tweets.slice(-10));
+  res.status(200).send(tweets.slice(-10).reverse());
+});
+
+//Get User Tweets
+app.get("/tweets/:username", (req, res) => {
+  const { username } = req.params;
+  const userTweets = tweets
+    .filter((tweet) => tweet.username === username)
+    .reverse();
+
+  if (!userTweets) {
+    res.status(400).send("No tweets found.");
+    return;
+  }
+
+  res.status(200).send(userTweets);
 });
 
 app.listen(5000, () => {
